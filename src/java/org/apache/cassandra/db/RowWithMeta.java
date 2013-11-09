b07e06ffcf16ff284a66b2efe76df2c31b2bb3c6
Satya <satya@tuplejump.com>
2013-11-09 14:28:37 +0530

    Simplifying for ASM

---
+++ src/java/org/apache/cassandra/db/RowWithMeta.java
@@ -0,0 +1,39 @@
+package org.apache.cassandra.db;
+
+import org.apache.cassandra.io.IColumnSerializer;
+import org.apache.cassandra.service.StorageService;
+import org.apache.cassandra.utils.ByteBufferUtil;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * User: satya
+ */
+public class RowWithMeta extends Row {
+    RowMeta rowMeta;
+
+    public RowWithMeta(DecoratedKey key, ColumnFamily cf, RowMeta rowMeta) {
+        super(key, cf);
+        this.rowMeta = rowMeta;
+    }
+
+    public static class RowWithMetaSerializer extends RowSerializer {
+        public void serialize(Row row, DataOutput dos, int version) throws IOException {
+            ByteBufferUtil.writeWithShortLength(row.key.key, dos);
+            ColumnFamily.serializer.serialize(row.cf, dos, version);
+            RowMeta.serializer.serialize(row.rowMeta, dos, version);
+        }
+
+        public Row deserialize(DataInput dis, int version, IColumnSerializer.Flag flag, ISortedColumns.Factory factory) throws IOException {
+            return new Row(StorageService.getPartitioner().decorateKey(ByteBufferUtil.readWithShortLength(dis)),
+                    ColumnFamily.serializer.deserialize(dis, flag, factory, version), RowMeta.serializer.deserialize(dis, version));
+        }
+
+        public long serializedSize(Row row, int version) {
+            int keySize = row.key.key.remaining();
+            return TypeSizes.NATIVE.sizeof((short) keySize) + keySize + ColumnFamily.serializer.serializedSize(row.cf, TypeSizes.NATIVE, version) + RowMeta.serializer.serializedSize(row.rowMeta, version);
+        }
+    }
+}
